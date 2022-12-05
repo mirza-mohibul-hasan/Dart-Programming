a91d83d69d142270eb2c9768f5299ae0349e7420
mirza-mohibul-hasan <73751848+mirza-mohibul-hasan@users.noreply.github.com>
2022-12-05 16:15:36 +0600

    Add files via upload

---
+++ Comments.dart
@@ -0,0 +1,7 @@
+void main(){
+  print("Hello Refat");
+  //printing hello world
+  print("We can also print multiple line using dart");
+  /*Hello
+  */
+}
---
+++ String.dart
@@ -0,0 +1,6 @@
+//Define String Literals
+String s1 = 'Single';
+String s2 = "Double";
+//String s3 ='It's Easy';
+String s3 ='It\'s Easy';
+String s4 = "It's Doubl";
